d665d56f2a3a417b3874c468870c67f7661241fa
titan_exasaur <tinytachyon14341@gmail.com>
2026-05-24 19:48:56 +0530

    Completed Project Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='customer-support-triage',
+    author='titan-exasaur',
+    author_email='tinytachyon14341@gmail.com',
+    version='0.1.0',
+    packages=find_packages()
+)
