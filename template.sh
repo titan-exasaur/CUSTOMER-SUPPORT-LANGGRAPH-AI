03c435af8ce8662420f7d524448f0a3ed24a97f7
titan_exasaur <tinytachyon14341@gmail.com>
2026-05-24 19:40:59 +0530

    Creates Project Template

---
+++ template.sh
@@ -0,0 +1,15 @@
+# creating directories
+mkdir 'notebooks/'
+mkdir 'src/'
+
+
+# creating files
+touch .gitignore
+touch requirements.txt
+touch .env
+touch setup.py
+
+touch 'src/__init__.py'
+touch 'src/app.py'
+
+touch 'notebooks/research.ipynb'
